4a42f3b5853e675ba74a35841100239993e53acf
Maxime Demolin <akbarova.armia@gmail.com>
2013-01-17 23:18:15 +0100

    rewrite specs

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,30 +1,24 @@
 require 'spec_helper'
 
 describe "StaticPages", :type => :controller do
-  render_views
+
+  subject { page }
 
   describe "Home page" do
+    before { visit root_path }
 
-    it "should have the h1 'Sample App'" do
-      visit root_path
-      page.should have_selector('h1',
-                                :text => "Welcome to the Sample App")
-    end
+    it { should have_selector('h1', :text => "Sample App") }
 
     # it "should have the title 'Home'" do
-    #   visit '/static_pages/home'
     #   page.should have_selector('title',
     #                             :text => "Ruby on Rails Tutorial Sample App | Home")
     # end
   end
 
   describe "Help page" do
+    before { visit help_path }
 
-    it "should have the h1 'Help'" do
-      visit help_path
-      page.should have_selector('h1',
-                                :text => "Help")
-    end
+    it { should have_selector('h1', :text => "Help") }
 
     # it "should have the title 'Help'" do
     #   visit '/static_pages/help'
@@ -34,12 +28,9 @@ describe "StaticPages", :type => :controller do
   end
   
   describe "About page" do
+    before { visit about_path }
     
-    it "should have the h1 'About Us'" do
-      visit about_path
-      page.should have_selector('h1',
-                                :text => 'About Us')
-    end
+    it { should have_selector('h1', :text => 'About Us') }
 
     # it "should have the title 'About Us'" do
     #   visit '/static_pages/about'
@@ -49,12 +40,9 @@ describe "StaticPages", :type => :controller do
   end
 
   describe "Contact page" do
+    before { visit contact_path }
 
-    it "should have the h1 'Contact'" do
-      visit contact_path
-      page.should have_selector('h1',
-                                :text => 'Contact')
-    end
+    it { should have_selector('h1', :text => 'Contact') }
   end
 
 end
