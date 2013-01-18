30858f107e0677a943534e10bcf128d873358d61
Maxime Demolin <akbarova.armia@gmail.com>
2013-01-18 19:40:53 +0100

    add test for email.downcase and refator

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -79,6 +79,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email adress with mixed case" do
+    let(:mixed_case_email) { "fOo@ExAmPlE.cOm" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   # password
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
